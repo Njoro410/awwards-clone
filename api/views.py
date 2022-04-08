525683c9032f45059c57bcf37eb1a42be8b8879a
njoro410 <brian.njoroge@student.moringaschool.com>
2022-04-09 02:09:07 +0300

    added views function to get user profile

--- api/views.py
+++ api/views.py
@@ -5,7 +5,14 @@ from rest_framework.decorators import api_view
 
 
 #views here
+@api_view
 def getProjects(request):
     projects = Projects.objects.all()
     serializer = ProjectsSerializer(projects, many = True)
+    return Response(serializer.data)
+
+
+def getProfile(request,pk):
+    profile = Profile.objects.get(id=pk)
+    serializer = ProfileSerializer(profile, many = False)
     return Response(serializer.data)
