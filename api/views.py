a2f83c68411ba9db0651e1da443cee3c76e3a472
njoro410 <brian.njoroge@student.moringaschool.com>
2022-04-09 02:11:35 +0300

    added view method to get specific projects according to pk

--- api/views.py
+++ api/views.py
@@ -6,13 +6,19 @@ from rest_framework.decorators import api_view
 
 #views here
 @api_view
-def getProjects(request):
+def GetProjects(request):
     projects = Projects.objects.all()
     serializer = ProjectsSerializer(projects, many = True)
     return Response(serializer.data)
 
-
-def getProfile(request,pk):
+@api_view
+def GetProfile(request,pk):
     profile = Profile.objects.get(id=pk)
     serializer = ProfileSerializer(profile, many = False)
+    return Response(serializer.data)
+
+@api_view
+def GetSpecificProject(request,pk):
+    article = Projects.objects.get(id=pk)
+    serializer = ProfileSerializer(article, many = False)
     return Response(serializer.data)
