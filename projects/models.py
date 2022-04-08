8d2309d6f1525b714de8eff64664953d50a0858c
njoro410 <brian.njoroge@student.moringaschool.com>
2022-04-08 22:40:37 +0300

    added extended  profile model from user model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,15 @@
+from email.policy import default
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField()
+    profilepic = models.ImageField(upload_to = 'profile_pic/', default = 'Profile pic', blank = True)
+    projects = models.ForeignKey('Projects', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f'{self.user.username} Profile'
+    
