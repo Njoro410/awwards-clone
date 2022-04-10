ff84779f307d22445a44f618dc946121a7486363
njoro410 <brian.njoroge@student.moringaschool.com>
2022-04-11 01:39:39 +0300

    added rating and user serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,13 +1,24 @@
 from rest_framework import serializers
-from projects.models import Profile,Projects
+from projects.models import Profile,Projects,Rating
+from django.contrib.auth.models import User
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+        
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = '__all__'
         
-
 class ProjectsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Projects
-        fields = '__all__'        
+        fields = '__all__'     
+        
+class RatingSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Rating
+        fields = '__all__'   
