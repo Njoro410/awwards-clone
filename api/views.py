6e5f33323c81ba5bdfb524bcbb71f452058f72d3
njoro410 <brian.njoroge@student.moringaschool.com>
2022-04-11 01:40:44 +0300

    added different views for the various api endpoints

--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
-from projects.models import Projects,Profile
-from .serializers import ProfileSerializer,ProjectsSerializer
+from projects.models import Projects,Profile,Rating
+from .serializers import ProfileSerializer,ProjectsSerializer,RatingSerializer, UserSerializer
+from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
@@ -12,13 +13,31 @@ def GetProjects(request):
     return Response(serializer.data)
 
 @api_view(['GET'])
+def GetUserDetails(request,pk):
+    profile = User.objects.get(id=pk)
+    serializer = UserSerializer(profile, many = False)
+    return Response(serializer.data)
+
+@api_view(['GET'])
 def GetProfile(request,pk):
     profile = Profile.objects.get(id=pk)
     serializer = ProfileSerializer(profile, many = False)
     return Response(serializer.data)
 
 @api_view(['GET'])
-def GetSpecificProject(request,pk):
-    article = Projects.objects.get(id=pk)
-    serializer = ProfileSerializer(article, many = False)
+def GetSpecificProject(request,id):
+    article = Projects.objects.get(id=id)
+    serializer = ProjectsSerializer(article, many = False)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def GetSpecificRating(request,pk):
+    rating = Rating.objects.filter(project_id=pk)
+    serializer = RatingSerializer(rating, many = True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def GetUserProjects(request,pk):
+    projects = Projects.objects.filter(owner_id=pk)
+    serializer = ProjectsSerializer(projects, many = True)
     return Response(serializer.data)
