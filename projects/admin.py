b3778cd4b15f5cea765d091f39fd297d2e1e23bf
njoro410 <brian.njoroge@student.moringaschool.com>
2022-04-11 01:46:53 +0300

    registered models

--- projects/admin.py
+++ projects/admin.py
@@ -1,8 +1,9 @@
 import profile
 from django.contrib import admin
-from .models import Profile,Projects,Rating
+from .models import Profile,Projects,Rating,Reviews
 # Register your models here.
 
 admin.site.register(Profile)
 admin.site.register(Projects)
 admin.site.register(Rating)
+admin.site.register(Reviews)
