972b273f937619d63619bb4dba24c5c9211de860
njoro410 <brian.njoroge@student.moringaschool.com>
2022-04-11 01:49:10 +0300

    added url paths to display data

--- projects/urls.py
+++ projects/urls.py
@@ -1,9 +1,16 @@
+from unicodedata import name
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
-    
+    path('',views.Index, name='home'),
+    path('addproject/',views.addProject, name='addproject'),
+    path('projectdetails/<str:id>/',views.projectDetails,name='projectdetails'),
+    path('addreview/<str:id>',views.addreview,name='addreview'),
+    path('profile/<str:id>/',views.uProfile,name="profile"),
+    path('search/', views.Search, name='search_results')
 ]
 
 if settings.DEBUG:
