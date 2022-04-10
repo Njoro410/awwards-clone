322164560b169866d266d1aa2f67e4a3b5eb0dde
njoro410 <brian.njoroge@student.moringaschool.com>
2022-04-11 01:49:55 +0300

    added a profile, projects and ratings view

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,98 @@
-from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, render
+from .models import Projects, Rating, Reviews,Profile
+from .forms import ProjectsForm, ReviewForm
+from django.views import View
+import requests
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+
+def Index(request):
+    # projects = getprojects()
+    response = requests.get('http://127.0.0.1:8000/api/allprojects/')
+
+    projects = response.json()
+
+    return render(request, 'index.html', {'projects': projects})
+
+@login_required
+def addProject(request):
+    form = ProjectsForm(request.POST, request.FILES or None)
+    if request.method == 'POST':
+        # form = ProjectsForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.owner = request.user
+            project.save()
+            return HttpResponseRedirect('/')
+    return render(request, 'addProject.html', {'form': form})
+
+
+def projectDetails(request, id):
+    project = Projects.objects.get(id=id)
+    response = requests.get(
+        'http://127.0.0.1:8000/api/specificProject/{}/'.format(id))
+    # convert reponse data into json
+    details = response.json()
+    rating = Rating.objects.filter(project_id=id)
+    reviews = Reviews.objects.filter(project_id=id)
+    total = 0
+    for i in rating:
+
+        total += (i.usability + i.design + i.content)/3
+        # t = float("{:.1f}".format(total))
+    # print(total)
+
+    return render(request, 'projectdetails.html', {'details': details, 'rating': rating, 'total': total, 'reviews': reviews, 'project':project})
+
+@login_required
+def addreview(request, id):
+    form = ReviewForm(request.POST or None)
+    if request.method == 'POST':
+        # form = ProjectsForm(request.POST, request.FILES)
+        content = request.POST['content']
+        usability = request.POST['usability']
+        design = request.POST['design']
+        user = request.user
+        project_id = id
+        Rating.objects.create(content=content, design=design,
+                              usability=usability, user=user, project_id=project_id)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.user = request.user
+            review.project_id = id
+            review.save()
+
+            return HttpResponseRedirect('index.html')
+
+    return render(request, 'addreview.html', {'form': form})
+
+@login_required
+def uProfile(request,id):
+    response = requests.get('http://127.0.0.1:8000/api/userDetails/{}/'.format(id))
+    details = response.json()
+    response2 = requests.get('http://127.0.0.1:8000/api/userProjects/{}/'.format(id))
+    projects = response2.json()
+    response3 = requests.get('http://127.0.0.1:8000/api/profile/{}/'.format(id))
+    data = response3.json()
+    user = request.user
+
+    
+    
+    return render(request, 'profile.html', {'details':details,'projects': projects,'user': user,'data':data} )
+
+
+def Search(request):
+
+    if 'title' in request.GET and request.GET["title"]:
+        search_term = request.GET.get("title")
+        projects = Projects.objects.filter(title=search_term)
+        
+        message = f"{search_term}"
+
+        return render(request, 'search.html', {"message": message, "projects": projects})
+
+    else:
+        message = "You haven't searched for any project"
+        return render(request, 'search.html', {"message": message})
